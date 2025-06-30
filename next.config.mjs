646fac841657ae3b8efa04d25002ecada5ed7512
Toby <latebloomerguy99@gmail.com>
2025-06-30 21:36:40 +0700

    Add compiler to remove logs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
+  compiler: {
+    removeConsole: {
+      exclude: ["error"], // keep console.error
+    },
+  },
 };
 
 export default nextConfig;
